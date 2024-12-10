0ef91a1f69eb8cbb07ce1b7d7518d1ff58c9ff4f
Bohdan Pryhkhodko <bohdanprykhodko36@gmail.com>
2024-12-11 00:31:00 +0200

    added category schema and endpoints

--- app/models.py
+++ app/models.py
@@ -4,4 +4,11 @@ def create_user_schema():
         "name": ""
     }
 
+def create_category_schema():
+    return {
+        "id": None,  # unique identifier for the category
+        "name": ""  # name of the category
+    }
+
 users = []
+categories = []
--- app/views.py
+++ app/views.py
@@ -17,3 +17,15 @@ def create_user():
 @app.route('/users', methods=['GET'])
 def get_users():
     return jsonify({"users": users})
+
+# Category Endpoints
+@app.route('/categories', methods=['POST'])
+def create_category():
+    category = request.json
+    category['id'] = len(categories) + 1
+    categories.append(category)
+    return jsonify({"message": "Category created", "category": category}), 201
+
+@app.route('/categories', methods=['GET'])
+def get_categories():
+    return jsonify({"categories": categories})
