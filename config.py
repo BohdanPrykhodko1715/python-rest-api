de25ab85c6f7ce570ae5cf9bdc054f95cf0f2b45
Bohdan Pryhkhodko <bohdanprykhodko36@gmail.com>
2024-12-15 16:04:08 +0200

    updated config to use postgres on heroku

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    uri = os.getenv("HEROKU_POSTGRESQL_COBALT_URL") or os.getenv("DATABASE_URL")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
     API_TITLE = "Expenses tracking API"
