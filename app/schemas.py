af8a996630abb9304d387ec11fd145a37d7f72ac
Bohdan Pryhkhodko <bohdanprykhodko36@gmail.com>
2024-12-15 16:32:58 +0200

    Lab3-Currency functionality: added schemas

--- app/schemas.py
+++ app/schemas.py
@@ -15,3 +15,8 @@ class RecordSchema(Schema):
     category_id = fields.Int(required=True)
     date = fields.DateTime(required=True)
     amount = fields.Float(required=True, validate=lambda x: x > 0)
+
+class CurrencySchema(Schema):
+    id = fields.Int(dump_only=True)
+    code = fields.Str(required=True, validate=validate.Length(equal=3))
+    name = fields.Str(required=True)
