7337bc46a7bfa6a2d3296c0c3b12cbc058a393ff
Bohdan Pryhkhodko <bohdanprykhodko36@gmail.com>
2024-12-15 14:08:10 +0200

    upadted __init__.py to use new approaches, added pyton-dotenv library and updated docker-compose to allow local conections

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,22 @@
 from flask import Flask
+from flask_smorest import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app import views
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    api = Api(app)
+
+    from app.views import bp
+    api.register_blueprint(bp)
+
+    return app
+
